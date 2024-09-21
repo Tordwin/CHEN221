4b60936f500c4eff465626258b4672aad20cec53
Edwin Chen <ec7233@g.rit.edu>
2024-09-20 23:35:51 -0400

    .

--- Script 01/ping_test.py
+++ Script 01/ping_test.py
@@ -2,38 +2,63 @@
 # Student Name: Edwin Chen
 # Date: 9/21/2024
 
-import subprocess
 import os
 
+# default gateway function
 def get_default_gateway():
-    p = subprocess.Popen(["ip r"], stdout=subprocess.PIPE, shell=True)
-    out = p.stdout.read()
-    print(out)
+    print()
+    # Writes in terminal the command and runs it
+    os.system("ip r | grep default | awk '{print $3}'")
 
+# Pings local server
 def test_local():
-    return
+    print()
+    try:
+        # Writes in terminal the command
+        os.system('ping -c 4 127.0.0.1')
+    except Exception as e:
+        print("An error has occured: " + str(e))
+    print()
 
+# Pings RIT's DNS server
 def test_remote():
-    return
+    print()
+    try:
+        # Writes in terminal the command and runs it
+        os.system('ping -c 4 129.21.3.17')
+    except Exception as e:
+        print("An error has occured: " + str(e))
+    print()
 
 def test_DNS():
-    return
+    print()
+    try:
+        # Writes in terminal the command and runs it
+        os.system('ping -c 4 google.com')
+    except Exception as e:
+        print("An error has occured: " + str(e))
+    print()
 
 def main():
+    # Clears the terminal
     os.system("clear")
+    # While loop keeps the user options displayed after
+    # it is run or until option 5 is selected.
     while True:
+        # User Options
         print("Ping Test Menu")
         print("1. Display the default gateway")
         print("2. Test Local Connectivity")
         print("3. Test Remote Connectivity")
         print("4. Test DNS Resolution ")
         print("5. Exit/quit the script")
-
+        # Prompts the user with the question and grabs it as an input
         option = input("Type the number of the option you would like to start: ")
-
-        if option.isnumeric() == True:
+        # Checks if it is a number and only allows 1-5
+        if option.isnumeric() == True and 0 < int(option) < 6:
             if option == "1":
                 get_default_gateway()
+                print()
             if option == "2":
                 test_local()
             if option == "3":
@@ -45,10 +70,6 @@ def main():
                 break
         else:
             print("\n'" + option + "' is not an option.\n")
-            
-
-    return
-
 
 if __name__ == "__main__":
     main()
