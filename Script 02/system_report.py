289e11fe9a0bf2f229614b32a34728b40c12af86
Edwin Chen <ec7233@g.rit.edu>
2024-10-02 15:52:45 -0400

    Fixing script

--- Script 02/system_report.py
+++ Script 02/system_report.py
@@ -6,57 +6,75 @@ import os
 import platform
 import subprocess
 
+def run_command(com):
+    run = subprocess.run(com, capture_output=True, text=True)
+    return run.stdout.strip()
+
 def main():
-    output = subprocess.run(['date'], capture_output=True, text=True)
-    date = output.stdout.strip()
+    # Date & Time
+    date = run_command('date')
+
+    # Log File
+    log = "/home/student/Scripts/script02_system_report.log"
+
+    # Clears the terminal screen when starting the script
     os.system("clear")
+
+    # Prints date & time
     print("\033[31m          System Report - " + date + "\033[0m")
 
-    hostname = ""
-    domain = ""
-    print("\n\033[1;32mDevice Information\033[0m")
-    print("Hostname: " + hostname)
-    print("Domain: " + domain)
-    
-    ip = ""
-    gateway = ""
-    netmask = ""
-    dns1 = ""
-    dns2 = ""
-    print("\n\033[1;32mNetwork Information\033[0m")
-    print("IP address: ")
-    print("Gateway: ")
-    print("Network Mask: ")
-    print("DNS1: ")
-    print("DNS2: ")
+    with open(log, 'w') as file:
+        
+        def writer(message):
+            print(message)
+            file.write(message)
 
-    oper_sys = ""
-    osv = ""
-    kernal = ""
-    print("\n\033[1;32mOS Information\033[0m")
-    print("Operating System: ")
-    print("Operating Version: ")
-    print("Kernal Version: ")
-    
-    storage = ""
-    aval_space = ""
-    print("\n\033[1;32mStorage Information\033[0m")
-    print("Hard Drive Capacity: ")
-    print("Avaliable Space: ")
+        linux_hostname = subprocess.check_output("hostname -f", shell=True).decode().strip()
+        hostname = linux_hostname.split('.')[0]
+        domain = '.'.join(linux_hostname.split('.')[1:])
+        writer("\n\033[1;32mDevice Information\033[0m")
+        writer("Hostname: " + hostname)
+        writer("Domain: " + domain)
+        
+        ip = ""
+        gateway = ""
+        netmask = ""
+        dns1 = ""
+        dns2 = ""
+        writer("\n\033[1;32mNetwork Information\033[0m")
+        writer("IP address: ")
+        writer("Gateway: ")
+        writer("Network Mask: ")
+        writer("DNS1: ")
+        writer("DNS2: ")
 
-    cpu = ""
-    cpu_proc = ""
-    cpu_core = ""
-    print("\n\033[1;32mProcessor Information\033[0m")
-    print("CPU Model: ")
-    print("Number of processors: ")
-    print("Number of cores: ")
-    
-    tot_ram = ""
-    aval_ram = ""
-    print("\n\033[1;32mMemory Information\033[0m")
-    print("Total Ram: ")
-    print("Avaliable Ram: ")
+        oper_sys = ""
+        osv = ""
+        kernal = ""
+        writer("\n\033[1;32mOS Information\033[0m")
+        writer("Operating System: ")
+        writer("Operating Version: ")
+        writer("Kernal Version: ")
+        
+        storage = ""
+        aval_space = ""
+        writer("\n\033[1;32mStorage Information\033[0m")
+        writer("Hard Drive Capacity: ")
+        writer("Avaliable Space: ")
+
+        cpu = ""
+        cpu_proc = ""
+        cpu_core = ""
+        writer("\n\033[1;32mProcessor Information\033[0m")
+        writer("CPU Model: ")
+        writer("Number of processors: ")
+        writer("Number of cores: ")
+        
+        tot_ram = ""
+        aval_ram = ""
+        writer("\n\033[1;32mMemory Information\033[0m")
+        writer("Total Ram: ")
+        writer("Avaliable Ram: ")
 
 if __name__ == "__main__":
     main()
