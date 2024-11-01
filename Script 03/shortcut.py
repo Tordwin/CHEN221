2913dce4013c4ac0f5f50aff8f1d0dea6e5c1f69
Edwin Chen <ec7233@g.rit.edu>
2024-11-01 00:05:25 -0400

    .

--- Script 03/shortcut.py
+++ Script 03/shortcut.py
@@ -7,16 +7,22 @@ import subprocess
 from pathlib import Path
 
 def create_link():
+    #Usr enters path for where to place symbolic link
     path = input("Enter the location where you would like to create a shortcut. ")
-
+    
+    #Finds the path by utilizing subprocess  using find command and captures output 
     file_path = subprocess.run(["find", "/", "-name", path], capture_output=True, text=True)
     final_path = file_path.stdout.strip()
     
+    #Usr enters shortcut name for link
     shortcut = input("Enter the name of the shortcut. ")
-    link_path = Path.home() / "Desktop" / shortcut
+    #Joining path
+    link_path = Path.home().joinpath("Desktop", shortcut)
 
     try:
+        print("\nCreating shortcut link...")
         os.symlink(final_path, link_path)
+        print("Linked shortcut has been created!\n")
     except FileExistsError:
         print("This shortcut already exists.")
     except FileNotFoundError:
@@ -26,20 +32,41 @@ def create_link():
 
 
 def delete_link():
+    #Usr enters shortcut name
     shortcut = input("Enter the name of the shortcut. ")
-    link_path = Path.home() / "Desktop" / shortcut
+    #Creates a path with the shortcut name to find where it is located
+    link_path = Path.home().joinpath("Desktop", shortcut)
 
     try:
+        #Checks if it is a symbolic link
         if link_path.is_symlink():
+            print("\nDeleting shortcut link...")
+            #Deletes link and shortcut
             link_path.unlink()
-            print("Path has been unlinked.")
+            print("Linked shortcut deleted!\n")
         else:
             print("Filepath not found. ")
     except Exception as e:
         print("An error has occured " + e + ".")
 
 def locate_link():
-    return
+    #Joining path
+    path = Path.home().joinpath("Desktop")
+    #List for filepaths that are symbolic
+    filepaths = []
+    #Loop that checks for symbolic links and appends to filepaths list
+    for i in path.iterdir():
+        if i.is_symlink():
+            filepaths.append(i)
+    
+    #Prints symbolic links, locations, and how many links there is
+    if filepaths:
+        print("\nSymbolic links:\n")
+        for files in filepaths:
+            print(files.name + " -> " + str(files.resolve()) + "\n")
+        print("There is/are " + str(len(filepaths)) + " symbolic links.\n")
+    else:
+        print("\nNo symbolic links found.\n")
 
 
 def main():
@@ -51,7 +78,7 @@ def main():
         print("3. Find a shortcut.")
         print("Type 'quit' to exit this program.")
         
-        usr_input = input("Which option do you choose? ")
+        usr_input = input("\nWhich option do you choose? ")
         if usr_input == "1":
             create_link()
         elif usr_input == "2":
@@ -59,10 +86,11 @@ def main():
         elif usr_input == "3":
             locate_link()
         elif usr_input == "quit":
+            os.system("clear")
             print("Quitting...")
             break
         else:
-            print("Please input a valid choice between 1 and 3. ")
+            print("\nPlease input a valid choice between 1 and 3. ")
 
 
 if __name__ == "__main__":
